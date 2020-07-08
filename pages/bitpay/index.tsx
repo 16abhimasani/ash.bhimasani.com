33dca50c609d4f1ac401ba165f9a97a06b970c1f
Ash Bhimasani <abhimasani@bitpay.com>
2020-07-08 17:45:55 -0400

    set caption + icons for bp page

--- pages/bitpay/index.tsx
+++ pages/bitpay/index.tsx
@@ -39,44 +39,44 @@ const BitPay: React.FC = () => (
       link="/bitpay/extension"
     />
     <PostBox
-      icon="/logos/bitpay-qr.svg"
+      icon="/logos/bitpay-card.svg"
       title="Invoice V4"
-      caption="Our flagship payment gateway used by major merchants"
+      caption="Flagship payment gateway powering the network"
       date="Aug 2019 ~ Present"
       link="/bitpay/invoice"
     />
     <PostBox
-      icon="https://bitpay.com/img/wallet-logos/bitpay-wallet.svg"
+      icon="/logos/bitpay-id.svg"
       title="BitPay ID"
-      caption="Our consumer product suite with KYC / AML"
+      caption="Consumer product suite + KYC / AML"
       date="Apr 2019 ~ Aug 2019"
       link="/bitpay/id"
     />
     <PostBox
-      icon="https://bitpay.com/img/wallet-logos/bitpay-wallet.svg"
+      icon="/logos/bitpay-globe.svg"
       title="BitPay Directory"
       caption="A curated directory of where to use crypto + promos"
       date="Mar 2019 ~ Apr 2019"
       link="/bitpay/directory"
     />
     <PostBox
-      icon="https://bitpay.com/img/wallet-logos/bitpay-wallet.svg"
+      icon="/logos/bitpay-emails.svg"
       title="Emails"
-      caption="A curated directory of where to use crypto + promos"
+      caption="Transactional emails for merchants & payers"
       date="Aug 2018 ~ Jan 2019"
       link="/bitpay/email"
     />
     <PostBox
-      icon="https://bitpay.com/img/wallet-logos/bitpay-wallet.svg"
+      icon="/logos/bitpay-site.svg"
       title="Website"
-      caption="A curated directory of where to use crypto + promos"
+      caption="Landing, operational, & product pages"
       date="Aug 2018 ~ Jan 2019"
       link="/bitpay/site"
     />
     <PostBox
-      icon="https://bitpay.com/img/wallet-logos/bitpay-wallet.svg"
+      icon="/logos/bitpay-support.svg"
       title="Support Desk"
-      caption="A curated directory of where to use crypto + promos"
+      caption="A streamlined customer service platform"
       date="May 2018 ~ Aug 2018"
       link="/bitpay/support"
     />
