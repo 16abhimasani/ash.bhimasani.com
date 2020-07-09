c39cf5b86ad23267538b7bda9fd3905f5a5d1f80
Ash Bhimasani <abhimasani@bitpay.com>
2020-07-08 23:06:15 -0400

    add blockquote style and refactor sub pages

---
+++ pages/bitpay/extension/wallet.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const BitPayExtensionWallet: React.FC = () => (
+  <div className="body">
+    <a
+      href="https://bitpay.com/extension"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img width="100%" src="https://bitpay.com/assets/extension-banner.png" />
+    </a>
+    <div className="lead begin-section mb-0 end-section">
+      I'm really excited to share the latest project we've been working on; this
+      project was a concept brainstormed amongst engineers working on the gift
+      card program about 3 years ago and we finally had a chance to build.
+    </div>
+  </div>
+);
+
+export default BitPayExtensionWallet;
