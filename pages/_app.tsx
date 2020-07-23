67943cacbc793a7f0f4a58546998097a5d81a52e
Ash Bhimasani <abhimasani@bitpay.com>
2020-07-23 19:10:17 -0400

    lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,34 +8,32 @@ import "../styles/styles.scss";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 
-const AshBhimasani: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Head>
-        <title>Ash Bhimasani - Designer & Engineer</title>
-        <meta
-          property="description"
-          content="I am a product designer & software engineer with special interests in UX, growth hacking, and business strategy currently working at BitPay"
-          key="description"
-        />
-        <meta
-          property="twitter:image"
-          content="/banners/logo.jpg"
-          key="twitter-image"
-        />
-        <meta property="og:image" content="/banners/logo.jpg" key="og-image" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-      <div className="layout">
-        <Header />
-        <AnimatePresence>
-          <Component {...pageProps} />
-        </AnimatePresence>
-        <Footer />
-      </div>
-    </>
-  );
-};
+const AshBhimasani: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Ash Bhimasani - Designer & Engineer</title>
+      <meta
+        property="description"
+        content="I am a product designer & software engineer with special interests in UX, growth hacking, and business strategy currently working at BitPay"
+        key="description"
+      />
+      <meta
+        property="twitter:image"
+        content="/banners/logo.jpg"
+        key="twitter-image"
+      />
+      <meta property="og:image" content="/banners/logo.jpg" key="og-image" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+    <div className="layout">
+      <Header />
+      <AnimatePresence>
+        <Component {...pageProps} />
+      </AnimatePresence>
+      <Footer />
+    </div>
+  </>
+);
 
 export default AshBhimasani;
