49a1e7c5322ad4fcd0999d8ab20672adc8650272
Ash Bhimasani <abhimasani@bitpay.com>
2020-08-01 14:46:18 -0400

    add file, mobile optimize footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -6,15 +6,19 @@ const Header: React.FC = () => (
   <footer className={classes.footer}>
     <div className={classes.nav}>
       <Anchor content="Email" href="mailto:ash@bhimasani.com" />
-      <div className={classes.dot}>·</div>
-      <Anchor content="GitHub" href="https://github.com/16abhimasani" />
-      <div className={classes.dot}>·</div>
-      <Anchor content="Dribbble" href="https://dribbble.com/ash-bhimasani" />
+      <div className={classes.nav__hide}>
+        <div className={classes.dot}>·</div>
+        <Anchor content="GitHub" href="https://github.com/16abhimasani" />
+        <div className={classes.dot}>·</div>
+        <Anchor content="Dribbble" href="https://dribbble.com/ash-bhimasani" />
+      </div>
       <div className={classes.dot}>·</div>
       <Anchor
         content="LinkedIn"
         href="https://www.linkedin.com/in/ash-bhimasani-b98757139/"
       />
+      <div className={classes.dot}>·</div>
+      <Anchor content="Resume" href="/docs/resume.pdf" />
     </div>
     <div className={classes.copyright}>
       Made with 💖 by Ash Bhimasani © 2020
