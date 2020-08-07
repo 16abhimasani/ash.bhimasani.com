a6b10496c6b2fc987b986cd152690dddabf09c49
Ash Bhimasani <abhimasani@bitpay.com>
2020-08-06 23:42:16 -0400

    add analytics tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,22 @@ export default class extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-49532918-1"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-49532918-1');
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
