062d85ad1459c0a13c2ed23ba4aab4fafcbc980b
Ash Bhimasani <abhimasani@bitpay.com>
2020-08-08 13:06:27 -0400

    structure intro

--- pages/bitpay/invoice/select.tsx
+++ pages/bitpay/invoice/select.tsx
@@ -13,17 +13,46 @@ const BitPayInvoiceWalletSelection: React.FC = () => (
         <h1 className="begin-section pt-0">Wallet Selection</h1>
         <h5 className="end-section">User experience tailoring and analytics</h5>
         <WideScreen src="/imgs/invoice/select/wallet-selection.png" />
-        <div className="lead begin-section end-section">
+        <div className="lead begin-section">
           <p>
+            Up until recently, the
             <Anchor
-              content="BitPay Invoice"
+              content="Invoice"
               href="https://bitpay.com/demos"
+              spaceBefore
               spaceAfter
               bold
             />
-            obviously, the first part of any Authentication flow is the Account
-            Creation flow. This would be utilized whenever a user visits our
-            payment gateway when making a transaction over $3k USD.
+            was a fairly robust but rigid interface. It only needed to handle 2
+            currencies + a Payment Protocol link or QR code. Adding ETH and
+            ERC20 token capability became the first time we had to support
+            another payment method (Web3) where there was clear preference based
+            on wallet.
+          </p>
+          <p>
+            If the user is paying with a BitPay or Copay wallet, then the only
+            way to pay is with a PayPro link. However, if the user is paying
+            with MetaMask, then we need to connect to their wallet via Web3 to
+            pay. Without any upfront selection, we initially handled this by
+            detecting the presence of Web3 and providing a manual toggle (less
+            than ideal).
+          </p>
+          <blockquote>
+            It became clear after the launch of ETH that we needed a robust but
+            elegant way to handle rapidly diverging feature sets.
+          </blockquote>
+          <p>
+            In response to these problems, I created the Wallet Selection flow
+            over the course of 2019, drawing inspiration from companies like
+            <Anchor content="Plaid" href="https://plaid.com/" spaceBefore />,
+            <Anchor
+              content="Mollie"
+              href="https://www.mollie.com/"
+              spaceBefore
+            />
+            , and more. I finally had a chance to build and launch it in the
+            beginning of 2020 as the company made P2P support a major
+            initiative.
           </p>
         </div>
         <video
@@ -36,6 +65,31 @@ const BitPayInvoiceWalletSelection: React.FC = () => (
         >
           <source src="/vids/invoice/wallet-selection.mp4" type="video/mp4" />
         </video>
+        <div className="lead begin-section pt-0 end-section">
+          <p>
+            The solution was a relatively obvious one because of the following
+            requirements:
+          </p>
+          <ol className="list">
+            <li className="list__item">
+              <div className="list__item__number">1</div>
+              <div>Use Payment Protocol if supported</div>
+            </li>
+            <li className="list__item">
+              <div className="list__item__number">2</div>
+              <div>Use Web3 for all other ETH wallets</div>
+            </li>
+            <li className="list__item">
+              <div className="list__item__number">3</div>
+              <div>Display Address + Amount for P2P payments</div>
+            </li>
+          </ol>
+          <p>
+            However, because we now could now display custom behavior for any
+            particular payment method selected, the design ended up unlocking a
+            lot more capability than we initially set out to solve.
+          </p>
+        </div>
       </section>
 
       <section>
