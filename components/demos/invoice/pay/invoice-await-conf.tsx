43e52dbc9c21076d957c2f237618fdf8871fd93d
Ash Bhimasani <abhimasani@bitpay.com>
2020-12-23 10:23:57 -0500

    add btc explorer link to Invoice Awaiting Demo

--- components/demos/invoice/pay/invoice-await-conf.tsx
+++ components/demos/invoice/pay/invoice-await-conf.tsx
@@ -22,7 +22,14 @@ const InvoiceAwaitingConfDemo: React.FC<{ type: string }> = ({ type }) => {
         >
           <div className={invoiceClasses.title}>Awaiting Confirmation</div>
           <div className={invoiceClasses.caption}>
-            We are awaiting confirmation of your payment on the blockchain.
+            We are awaiting confirmation of your payment on the{" "}
+            <a
+              href="https://explorer.bitcoin.com/btc/tx/29a3efd3ef04f9153d47a990bd7b048a4b2d213daaa5fb8ed670fb85f13bdbcf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              blockchain
+            </a>
           </div>
           <InvoicePulser
             icon={`/icons/currencies/${type.toLocaleLowerCase()}.svg`}
