fe62f44dbfa752832bbfe8ea4feddb77fce5f216
Ash Bhimasani <16abhimasani@gmail.com>
2021-02-08 16:42:29 -0500

    improve dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,11 @@ export const ThemeContext = React.createContext({ dark: false });
 const AshBhimasani: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [theme, setTheme] = useState({ dark: false });
   useEffect(() => {
+    const html = document.getElementsByTagName("html")[0]?.classList;
     const body = document.body.classList;
-    theme.dark ? body.add("dark-mode") : body.remove("dark-mode");
+    theme.dark
+      ? (body.add("dark-mode"), html.add("black-out"))
+      : (body.remove("dark-mode"), html.remove("black-out"));
   }, [theme]);
   return (
     <>
