ee05efdc038eaa87ff34a67b23ed2086b0bdecff
Ash Bhimasani <16abhimasani@gmail.com>
2021-02-09 00:12:37 -0500

    fix PostBox dark mode

--- components/post-box/post-box.tsx
+++ components/post-box/post-box.tsx
@@ -38,50 +38,51 @@ const PostBox: React.FC<{
 }> = ({ title, caption, link, icon, date, newTab, locked }) => {
   const theme = useContext(ThemeContext);
   const Box = (
-    <motion.a
-      whileHover={{ scale: 0.997 }}
-      whileTap={{ scale: 0.99 }}
-      className={cx({
-        box: true,
-        dark: theme.dark,
-      })}
-      style={{ pointerEvents: locked ? "none" : "initial" }}
-      href={link}
-      target={newTab ? "_blank" : "_self"}
-      rel={newTab ? "noopener noreferrer" : ""}
-    >
-      {icon && (
-        <motion.img
-          animate={
-            LogoConfigs.hasOwnProperty(icon) ? LogoConfigs[icon].animate : {}
-          }
-          transition={
-            LogoConfigs.hasOwnProperty(icon) ? LogoConfigs[icon].transition : {}
-          }
-          className={cx({
-            box__icon: true,
-            invert: LogoConfigs[icon]?.invertible && theme.dark,
-          })}
-          src={icon}
-          alt={title}
-        />
-      )}
-      <div>
-        <div className={classes.box__title}>{title}</div>
-        <div className={classes.box__caption}>{caption}</div>
-      </div>
-      {date && <div className={classes.box__date}>{date}</div>}
-      {locked && (
-        <div className={classes.box__locked}>
-          <img
-            className={classes.box__locked__icon}
-            src="/icons/padlock-white.svg"
-            alt="locked"
+    <motion.div className={cx({ dark: theme.dark })}>
+      <motion.a
+        whileHover={{ scale: 0.997 }}
+        whileTap={{ scale: 0.99 }}
+        className={classes.box}
+        style={{ pointerEvents: locked ? "none" : "initial" }}
+        href={link}
+        target={newTab ? "_blank" : "_self"}
+        rel={newTab ? "noopener noreferrer" : ""}
+      >
+        {icon && (
+          <motion.img
+            animate={
+              LogoConfigs.hasOwnProperty(icon) ? LogoConfigs[icon].animate : {}
+            }
+            transition={
+              LogoConfigs.hasOwnProperty(icon)
+                ? LogoConfigs[icon].transition
+                : {}
+            }
+            className={cx({
+              box__icon: true,
+              invert: LogoConfigs[icon]?.invertible && theme.dark,
+            })}
+            src={icon}
+            alt={title}
           />
-          Coming Soon
+        )}
+        <div>
+          <div className={classes.box__title}>{title}</div>
+          <div className={classes.box__caption}>{caption}</div>
         </div>
-      )}
-    </motion.a>
+        {date && <div className={classes.box__date}>{date}</div>}
+        {locked && (
+          <div className={classes.box__locked}>
+            <img
+              className={classes.box__locked__icon}
+              src="/icons/padlock-white.svg"
+              alt="locked"
+            />
+            Coming Soon
+          </div>
+        )}
+      </motion.a>
+    </motion.div>
   );
   return (
     <>{link.includes("://") ? <> {Box} </> : <Link href={link}>{Box}</Link>}</>
