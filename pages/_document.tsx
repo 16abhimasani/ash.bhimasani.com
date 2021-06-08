1c15348f8582fc2947a3ac7c2e3b42a9651a92f4
Ash Bhimasani <16abhimasani@gmail.com>
2021-06-08 15:27:29 -0500

    update pkgs, try fixing safari favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,8 +14,33 @@ export default class extends Document {
           />
           <link
             rel="apple-touch-icon"
+            sizes="60x60"
+            href="/favicon/apple-touch-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/favicon/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/favicon/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/favicon/apple-touch-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
             sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
+            href="/favicon/apple-touch-icon-180x180.png"
+          />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#FF85B4"
           />
           <link
             rel="icon"
@@ -30,13 +55,11 @@ export default class extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/favicon/safari-pinned-tab.svg"
-            color="#FF85B4"
-          />
-          <link rel="shortcut icon" href="/favicon/favicon.ico" />
           <meta name="msapplication-TileColor" content="#FF85B4" />
+          <meta
+            name="msapplication-TileImage"
+            content="/favicon/mstile-150x150.png"
+          />
           <meta
             name="msapplication-config"
             content="/favicon/browserconfig.xml"
